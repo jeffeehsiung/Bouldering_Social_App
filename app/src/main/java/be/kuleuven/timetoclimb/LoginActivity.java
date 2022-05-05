fce36beb966ea6240290d0ffce30102870ea99a1
kamielsabo <mandela.dehondt@gmail.com>
2022-05-05 16:35:50 +0200

    added login capability to login screen (temporary)

--- app/src/main/java/be/kuleuven/timetoclimb/LoginActivity.java
+++ app/src/main/java/be/kuleuven/timetoclimb/LoginActivity.java
@@ -8,22 +8,39 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class LoginActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     FloatingActionButton google;
     ProgressBar progressBar;
+    TextView txtUsername;
+    TextView txtPassword;
+    TextView lblError;
+    Button btnLoginAttempt;
+    User user;
 
     float opacityf;
 
@@ -38,6 +55,10 @@ public class LoginActivity extends AppCompatActivity {
         viewPager2 = findViewById(R.id.view_pager);
         google = findViewById(R.id.fab_google);
         progressBar = findViewById(R.id.progressBar);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnLoginAttempt = findViewById(R.id.btnLoginAttempt);
+        lblError = findViewById(R.id.lblError);
 
         tabLayout.addTab(tabLayout.newTab().setText("Login"));
         tabLayout.addTab(tabLayout.newTab().setText("Signup"));
@@ -62,6 +83,56 @@ public class LoginActivity extends AppCompatActivity {
         google.setAlpha(opacityf);
         tabLayout.setAlpha(opacityf);
         google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+
+    }
+
+    public void onBtnLoginAttempt_Click(View caller) {
+        // Create intent
+        Intent intentLogin = new Intent(this, MainActivity.class);
+
+        // Parse user input
+        String givenUsername = txtUsername.getText().toString();
+        String givenPassword = txtPassword.getText().toString();
+
+        // Check database
+        String requestURL = "https://studev.groept.be/api/a21pt411/getUserByUsername/" + givenUsername;
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        final JSONArray[] testResult = new JSONArray[1];
+        JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.GET, requestURL, null,
+
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String username = "";
+                        String password = "";
+                        try {
+                            username = response.getJSONObject(0).getString("username");
+                            password = response.getJSONObject(0).getString("password");
+                        } catch (JSONException e) {
+                            Log.e( "Login", e.getMessage(), e );
+                        }
+                        if(username.equals(givenUsername) && password.equals(givenPassword)) {
+                            lblError.setText("");
+                            user = new User(username, password);
+                            // Pass user with intent
+                            intentLogin.putExtra("username", user.getUsername());
+                            intentLogin.putExtra("password", user.getPassword());
+                            startActivity(intentLogin);
+                        }
+                        else {
+                            lblError.setText("Wrong password, please try again");
+                        }
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        lblError.setText("Username does no exist");
+                    }
+                }
+        );
+        requestQueue.add(submitRequest);
 
     }
 
--- app/src/main/java/be/kuleuven/timetoclimb/User.java
+++ app/src/main/java/be/kuleuven/timetoclimb/User.java
@@ -3,7 +3,17 @@ package be.kuleuven.timetoclimb;
 import java.util.ArrayList;
 
 public class User {
-    private String userName;
-    private String emailAdress;
+    private String username;
+    private String emailAddress;
+    private String password;
     private ArrayList<User> friends;
+
+    public User(String username, String emailAddress) {
+        this.username = username;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getUsername() {return username;}
+    public String getEmailAddress() {return emailAddress;}
+    public String getPassword() {return password;}
 }
