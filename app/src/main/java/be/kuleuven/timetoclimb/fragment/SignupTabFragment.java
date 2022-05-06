c2d82a4c9a5f825f946940b73cec71c7803c9276
jeffeehsiung <j22132235@gmail.com>
2022-05-06 16:27:05 +0200

    jeffee's commit: - build.gradle add implementation 'com.google.android.material:material:1.1.0-beta01' - add into manifest: <uses-permission android:name="android.permission.INTERNET" /> -add int build.gradle: implementation 'androidx.viewpager2:viewpager2:1.0.0' - add intent extra from login to main

--- app/src/main/java/be/kuleuven/timetoclimb/fragment/SignupTabFragment.java
+++ app/src/main/java/be/kuleuven/timetoclimb/fragment/SignupTabFragment.java
@@ -1,5 +1,6 @@
 package be.kuleuven.timetoclimb.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment;
 
 import org.json.JSONArray;
 
+import be.kuleuven.timetoclimb.MainActivity;
 import be.kuleuven.timetoclimb.R;
 import be.kuleuven.timetoclimb.dbConnection.DBConnector;
 import be.kuleuven.timetoclimb.dbConnection.ServerCallback;
@@ -82,7 +84,15 @@ public class SignupTabFragment extends Fragment {
             dbConnector.JSONRequest(databaseUrl, new ServerCallback() {
                 @Override
                 public void onSuccess(JSONArray jsonArrayResponse) {
-                    System.out.println(jsonArrayResponse.length());
+                    //create a Bundle object
+                    Bundle extras = new Bundle();
+                    //Adding key value pairs to this bundle
+                    extras.putString("username",strUser);
+                    extras.putString("password",strPass);
+
+                    Intent intentSignupSucceed= new Intent(getContext(), MainActivity.class);
+                    intentSignupSucceed.putExtras(extras);
+                    startActivity(intentSignupSucceed);
                 }
             });
 
