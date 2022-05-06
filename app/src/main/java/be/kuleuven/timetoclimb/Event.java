ed32503fd9e4c740899696b0b5b059f34a8a65d4
kamielsabo <mandela.dehondt@gmail.com>
2022-05-06 13:10:59 +0200

    Passed user from main activity to Calendar, then to EventCreator and ViewDate. Started making Event object and user behaviour.

--- app/src/main/java/be/kuleuven/timetoclimb/Event.java
+++ app/src/main/java/be/kuleuven/timetoclimb/Event.java
@@ -5,10 +5,47 @@ import java.util.ArrayList;
 
 public class Event {
     private String organiser;
+    private int climbingHallID;
+    private String description;
     private String title;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
 
+    public Event(String organiser, int climbingHallID, String description, String title, LocalDateTime startTime, LocalDateTime endTime) {
+        this.organiser = organiser;
+        this.climbingHallID = climbingHallID;
+        this.description = description;
+        this.title = title;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    /*
+        Getters and setters
+     */
+    public String getOrganiser() {
+        return organiser;
+    }
+
+    public void setOrganiser(String organiser) {
+        this.organiser = organiser;
+    }
+
+    public int getClimbingHallID() {
+        return climbingHallID;
+    }
+
+    public void setClimbingHallID(int climbingHallID) {
+        this.climbingHallID = climbingHallID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public String getTitle() {
         return title;
@@ -34,19 +71,5 @@ public class Event {
         this.endTime = endTime;
     }
 
-    public ArrayList<User> getInvitedPeople() {
-        return invitedPeople;
-    }
 
-    public void setInvitedPeople(ArrayList<User> invitedPeople) {
-        this.invitedPeople = invitedPeople;
-    }
-
-    public ArrayList<User> getAttendees() {
-        return attendees;
-    }
-
-    public void setAttendees(ArrayList<User> attendees) {
-        this.attendees = attendees;
-    }
 }
