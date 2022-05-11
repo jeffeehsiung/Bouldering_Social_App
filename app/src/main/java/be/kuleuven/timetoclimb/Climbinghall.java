f0544b7535b63641e5201264e1a9c50cae5b1605
jeffeehsiung <j22132235@gmail.com>
2022-05-11 23:34:06 +0200

    update ProfileActivity: - btnUpdate.setOnClickListener LoginTabFragment: - bundle extras added ("profileImage",profileImage) for Home.class DBConnector: - imageUploadRequest - imageRetrieveRequest

---
+++ app/src/main/java/be/kuleuven/timetoclimb/Climbinghall.java
@@ -0,0 +1,137 @@
+package be.kuleuven.timetoclimb;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Climbinghall {
+    private int id;
+    private String hallName;
+    private String address;
+    private Bitmap image;
+
+    public Climbinghall(int id, String hallName, String address) {
+        this.id = id;
+        this.hallName = hallName;
+        this.address = address;
+        //this.image = image; STILL NEEDS TO BE IMPLEMENTED AFTER TESTING!!!
+    }
+
+    private Climbinghall(JSONObject jsonObject) throws JSONException {
+        this.id = Integer.parseInt(jsonObject.getString("id"));
+        this.hallName = jsonObject.getString("hall_name");
+        this.address = jsonObject.getString("address");
+        this.image = null; // for now
+    }
+
+    // TO DO: doesn't work yet (after calling csrtctor with json returns null or smthng)
+    public static ArrayList<Climbinghall> DBContents(Context c) {
+        ArrayList<Climbinghall> climbinghalls = new ArrayList<>();
+
+        RequestQueue requestQueue = Volley.newRequestQueue(c);
+
+        String requestURL = "https://studev.groept.be/api/a21pt411/getAllClimbinghalls";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, requestURL, null,
+                new Response.Listener<JSONArray>()
+                {
+                    @Override
+                    public void onResponse(JSONArray response)
+                    {
+                        //make a copy of the response and store it
+                        try {
+                            //response pushed into parameter v in volley log, which can be access through external document.
+                            VolleyLog.v("Response:%n %s", response.toString(4));
+                            for(int i = 0; i < response.length(); i++) {
+                                climbinghalls.add(new Climbinghall(response.getJSONObject(i)));
+                            }
+                            System.out.println(climbinghalls.toString());
+                            System.out.println("Volley workyworky");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            System.out.println("Fail json");
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Log.d("Database" ,error.getLocalizedMessage(), error);
+                        System.out.println(error.getLocalizedMessage());
+                        System.out.println("Fail");
+                    }
+                }
+        );
+
+        return climbinghalls;
+    }
+
+    /*
+    Create bitmap from string
+     */
+    public Bitmap StringToBitMap(String encodedString){
+        try{
+            byte [] encodeByte = Base64.decode(encodedString,Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        }
+        catch(Exception e){
+            e.getMessage();
+            return null;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHallName() {
+        return hallName;
+    }
+
+    public void setHallName(String hallName) {
+        this.hallName = hallName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+}
