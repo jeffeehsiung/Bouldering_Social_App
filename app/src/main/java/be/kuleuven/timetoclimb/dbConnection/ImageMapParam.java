caf06a61814c1e6473da6905262f2f1016ad2b53
jeffeehsiung <j22132235@gmail.com>
2022-05-25 20:11:39 +0200

    todo: -Route create hall ID drop-down menu: 90%. adapter created correctly, dropdown list source "menuList" added correctly.
    Issue:
    when running the app, clicking the spinner, it gives error: Accessing hidden field Landroid/widget/AbsListView;->mIsChildViewEnabled:Z (unsupported, reflection, allowed)

--- app/src/main/java/be/kuleuven/timetoclimb/dbConnection/ImageMapParam.java
+++
@@ -1,9 +0,0 @@
-package be.kuleuven.timetoclimb.dbConnection;
-
-import org.json.JSONArray;
-
-import java.util.Map;
-
-public interface ImageMapParam {
-    void setParam(Map<String, String> params, String encodedImage);
-}
